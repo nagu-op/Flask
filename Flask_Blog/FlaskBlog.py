86c20ab18959e580d0034e87b5123ddffae6fa9e
op-coder <100160944+nagu-op@users.noreply.github.com>
2023-11-09 00:16:24 +0530

    Add files via upload

---
+++ Flask_Blog/FlaskBlog.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+posts= [
+    {'author':"raju",
+     'tittle':"blogpost1",
+     'content':"first post"
+     },{
+         'author':"revi",
+         'tittle':"blogpost2",
+         'content':"second Post"
+     }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts= posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title = "aboutPage")
+
+if __name__== '__main__':
+    app.run(debug = True)
